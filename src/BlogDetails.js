3f1ddcd70b323d03854f7e4ddc3277ce7c7252b6
Yosaire <96118628+Yosaire@users.noreply.github.com>
2023-03-31 13:48:02 +0200

    Working Page Details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,30 @@
 import { useParams } from "react-router-dom";
+import BlogList from "./BlogList";
+import { useEffect, useState } from "react";
 
 const BlogDetalis = () => {
+  const [article, showArticle] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/articles/" + id)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        showArticle(data);
+      });
+  });
+
   const { id } = useParams();
   return (
     <div className="blogDetails">
       <h1>Blog Details - {id}</h1>
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.context}</p>
+        </div>
+      )}
     </div>
   );
 };
